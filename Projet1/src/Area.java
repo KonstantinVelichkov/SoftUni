dffd8f0975ec79905a829a33121dd77d62b0ad04
KonstantinVelichkov <flipshot@gmail.com>
2020-12-01 00:48:45 +0200

    update

--- Projet1/src/Area.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Area {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int a= Integer.parseInt(scan.nextLine());
-        System.out.println(a*a);
-    }
-}
